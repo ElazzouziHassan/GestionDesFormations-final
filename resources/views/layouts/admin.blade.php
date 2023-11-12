896de98d40a3ec701ceca1bf04387bf23b6f01f9
wizardy <wizardhassan99@gmail.com>
2023-11-12 14:08:08 +0100

    admin layout

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    @livewireStyles
+  </head>
+<body>
+    <h1>Hello, World !</h1>
+
+    @livewireScripts
+</body>
+</html>
