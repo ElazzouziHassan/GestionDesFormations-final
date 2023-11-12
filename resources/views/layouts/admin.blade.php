98a3caaab8fa9f3b02f2dd0f4a07106095b7220f
wizardy <wizardhassan99@gmail.com>
2023-11-12 15:08:59 +0100

    commit

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="{{ asset('admin/vendors/base/vendor.bundle.base.css') }}">
     <link rel="stylesheet" href="{{ asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
     <link rel="stylesheet" href="{{ asset('admin/css/style.css') }}">
-    <link rel="shortcut icon" href="{{ asset('admin/images/favicon.png') }}" />
+    <link rel="shortcut icon" href="{{ asset('admin/images/logo-mini.png') }}" />
      
     @livewireStyles
   </head>
