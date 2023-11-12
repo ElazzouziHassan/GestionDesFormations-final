f4ef59c7ccfa84172308f81feed34a06744abddb
wizardy <wizardhassan99@gmail.com>
2023-11-12 14:50:43 +0100

    admin dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -13,7 +13,6 @@
         <div class="d-flex">
           <i class="mdi mdi-home text-muted hover-cursor"></i>
           <p class="text-muted mb-0 hover-cursor">&nbsp;/&nbsp;Dashboard&nbsp;/&nbsp;</p>
-          <p class="text-primary mb-0 hover-cursor">Analytics</p>
         </div>
       </div>
       <div class="d-flex justify-content-between align-items-end flex-wrap">
@@ -26,7 +25,6 @@
         <button type="button" class="btn btn-light bg-white btn-icon mr-3 mt-2 mt-xl-0">
           <i class="mdi mdi-plus text-muted"></i>
         </button>
-        <button class="btn btn-primary mt-2 mt-xl-0">Download report</button>
       </div>
     </div>
   </div>
