ad4e6352862471e88e35d658a6fec8bc7d7602ec
wizardy <wizardhassan99@gmail.com>
2023-11-13 20:00:56 +0100

    commit

--- app/Http/Requests/EtudiantFormRequest.php
+++ app/Http/Requests/EtudiantFormRequest.php
@@ -11,7 +11,7 @@ class EtudiantFormRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,13 @@ class EtudiantFormRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'cin' => ['required'],
+            'nom' => ['required'],
+            'prenom' => ['required'],
+            'date_naissance' => ['required'],
+            'adresse' => ['required'],
+            'adresse_email' => ['required'],
+            'numero_telephone' => ['required'],
         ];
     }
 }
