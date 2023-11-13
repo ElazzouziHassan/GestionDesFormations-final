87d351dd91b733c8c822d04a7abece749eb41dbc
wizardy <wizardhassan99@gmail.com>
2023-11-13 19:56:15 +0100

    commit

---
+++ resources/views/admin/etudiant/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="row">
+  <div class="col-md-12">
+    <div class="card">
+      <div class="card-header">
+        <h2>
+          Etudiants,
+          <a href="{{ url('admin/etudiant/create') }}" class="btn btn-primary btn-sm float-right">Add Etudiant</a>
+        </h2>
+      </div>
+      <div class="card-body">
+
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
