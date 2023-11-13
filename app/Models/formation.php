ecd37af968489bd50c7d4dcb811fecfa67c0d502
wizardy <wizardhassan99@gmail.com>
2023-11-13 19:09:20 +0100

    commit

--- app/Models/formation.php
+++ app/Models/formation.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class formation extends Model
 {
     use HasFactory;
+
+    protected $table = 'formation';
+
+    protected $fillable = [
+        'date_debut', 'prix', 'duree_en_mois', 'intitule', 'description', 'administrateur_id'
+    ];
+
+    public function administrateur() {
+        return $this->belongsTo('administrateur');
+    }
+
+    public function etudiants() {
+        return $this->hasMany('etudiant');
+    }
+
+    public function formateurs() {
+        return $this->hasMany('formateur');
+    }
 }
