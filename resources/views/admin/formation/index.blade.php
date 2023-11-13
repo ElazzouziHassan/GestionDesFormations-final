05f2f1fa8631b402e1f87320b59e44689c8086b0
wizardy <wizardhassan99@gmail.com>
2023-11-13 21:09:47 +0100

    commit

---
+++ resources/views/admin/formation/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="row">
+  <div class="col-md-12">
+    @if (session('message'))
+      <div class="alert alert-success">{{ session('message') }}</div>
+    @endif
+    <div class="card">
+      <div class="card-header">
+        <h2>
+          Formations,
+          <a href="{{ url('admin/formation/create') }}" class="btn btn-primary btn-sm float-right">Add Formation</a>
+        </h2>
+      </div>
+      <div class="card-body">
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
