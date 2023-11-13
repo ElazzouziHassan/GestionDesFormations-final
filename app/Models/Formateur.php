b2bf00be826eb859d9951614e05eabea4f37adda
wizardy <wizardhassan99@gmail.com>
2023-11-13 19:09:14 +0100

    commit

--- app/Models/Formateur.php
+++ app/Models/Formateur.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Formateur extends Model
 {
     use HasFactory;
+
+    protected $table = 'formateur';
+
+    protected $fillable = [
+        'cin', 'nom', 'prenom', 'date_naissance', 'adresse', 'adresse_email', 'numero_telephone'
+    ];
+
+    public function formations() {
+        return $this->belongsToMany('formation');
+    }
 }
